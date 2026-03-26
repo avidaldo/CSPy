e60ec278af2164e09784c9bcfa92bc71e1a6c137
Alejandro Vidal <avd.formacion@gmail.com>
2026-03-26 18:13:14 +0100

    Liam-hockey moved to other repo

--- Liam-hockey/app/domain/services.py
+++
@@ -1,7 +0,0 @@
-import math
-
-
-def calculate_save_percentage(total_saves: int, total_shots_received: int) -> float:
-    if total_shots_received == 0:
-        return math.nan
-    return total_saves / total_shots_received
